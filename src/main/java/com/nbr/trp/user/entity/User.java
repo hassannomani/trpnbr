c78b6204d912386a667a27d5286991881d18c1db
redsaiyanAlpha <nomanialvi@gmail.com>
2023-01-26 01:04:17 +0600

    model change and properties

--- src/main/java/com/nbr/trp/user/entity/User.java
+++ src/main/java/com/nbr/trp/user/entity/User.java
@@ -4,7 +4,10 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.sql.Date;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,15 +18,45 @@ import java.util.Set;
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(columnDefinition = "uniqueidentifier default newid()")
+    private String uuid;
 
-    private String username;
 
-    private String email;
+    @Column(name = "Username",nullable = false,unique = true)
+    public String Username;
 
-    private String password;
+    @Column(name = "Password")
+    public String Password;
 
+    @Column(name = "FirstName")
+    public String FirstName;
+
+    @Column(name = "LastName")
+    public String LastName;
+
+    @Column(name = "Email")
+    public String Email;
+
+    @Column(name = "AddedBy")
+    public String AddedBy;
+
+    @Column(name = "AddedDate")
+    @CreationTimestamp
+    public Date AddedDate;
+
+    @Column(name = "AddedFromIP")
+    public String AddedFromIP;
+
+    @Column(name = "UpdatedBy")
+    public String UpdatedBy;
+
+    @Column(name = "UpdatedDate")
+    @UpdateTimestamp
+    public Date  UpdatedDate;
+
+    @Column(name = "UpdatedFromIP")
+    public String UpdatedFromIP;
 
     @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinTable(
@@ -33,11 +66,15 @@ public class User {
     )
     private Set<Role> roles = new HashSet<>();
 
-    public User(String username, String email, String password, Set<Role> roles) {
+    public User(String username, String password, String Firstname, String Lastname, String email, String AddedBy, String AddedFromIp, Set<Role> roles) {
         //this.id = id;
-        this.username = username;
-        this.email = email;
-        this.password = password;
+        this.Username = username;
+        this.Email = email;
+        this.Password = password;
+        this.FirstName = Firstname;
+        this.LastName = Lastname;
+        this.AddedBy = AddedBy;
+        this.AddedFromIP = AddedFromIp;
         this.roles = roles;
     }
 
