9c3e35239be6663dad54bca1ad2e00fda4f7471d
Hassan Nomani Alvi <nomanialvi@gmail.com>
2024-06-27 16:26:32 +0600

    error status

---
+++ src/main/java/com/nbr/trp/user/entity/ErrorStatus.java
@@ -0,0 +1,27 @@
+package com.nbr.trp.user.entity;
+
+public class ErrorStatus {
+    int code;
+    String message;
+
+    public ErrorStatus(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- src/main/java/com/nbr/trp/user/filter/AuthEntryPointJwt.java
+++ src/main/java/com/nbr/trp/user/filter/AuthEntryPointJwt.java
@@ -1,13 +1,18 @@
 package com.nbr.trp.user.filter;
 
+import com.google.gson.Gson;
+import com.nbr.trp.common.entity.OTPResponseModel;
 import com.nbr.trp.common.service.CommonService;
 import com.nbr.trp.log.LoggerController;
+import com.nbr.trp.user.entity.ErrorStatus;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -34,7 +39,26 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
         String ip = commonService.getIPAddress(request);
         loggerController.UnAuthorized("Unauthorized access from "+ip);
 
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+        ErrorStatus err = new ErrorStatus(401,"Token Error");
+        String json = new Gson().toJson(err, ErrorStatus.class);
+        response.setStatus(401);
+        response.getWriter().write(json);
+        //response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
+
+    }
+
+    private void setUnauthorizedResponse(HttpServletResponse response, String reason) {
+        try {
+            ErrorStatus err = new ErrorStatus(401,reason);
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            String json = new Gson().toJson(err, OTPResponseModel.class);
+            response.setStatus(401);
+            response.getWriter().write(json);
+        } catch (IOException e) {
+            logger.error(String.format(e.getMessage()));
+        }
     }
 
 
